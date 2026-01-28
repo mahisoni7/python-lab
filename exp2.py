a62e3a07b3f238aed1c635ee4c56260438e3c139
mahisoni7 <mahisoni908@gmail.com>
2026-01-28 11:32:36 +0530

    lab

---
+++ exp2.py
@@ -0,0 +1,20 @@
+#part1
+# x1 = int(input("enter value x1:"))
+# x2 = int(input("enter value x2:"))
+# y1 = int(input("enter value y1:"))
+# y2 = int(input("enter value y2:"))
+# import math
+# d = math.sqrt((x2-x1)**2+(y2-y1)**2)
+# print("distance of points are:",d)
+
+ 
+# x1 = int(input("enter value x1:"))
+# x2 = int(input("enter value x2:"))
+# y1 = int(input("enter value y1:"))
+# y2 = int(input("enter value y2:"))
+# midx = ((x1 + x2)/2) 
+# midy = ((y1+y2)/2)
+# print((midx,midy))
+
+#part b of exp2 command line 
+
