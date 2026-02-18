d84540699c56fc86844d3e53e7313fa45672201e
mahisoni7 <mahisoni908@gmail.com>
2026-02-18 11:15:38 +0530

    neww

--- exp3.py
+++ exp3.py
@@ -2,14 +2,27 @@
 # if x
 
 
-x = int(input("enter:"))
-if x%5==0 and x%11== 0:
-    print("by both")
-elif x%5== 0:
-    print("by 5")
-elif x%11 == 0:
-    print("by 11")
+# x = int(input("enter:"))
+# if x%5==0 and x%11== 0:
+#     print("by both")
+# elif x%5== 0:
+#     print("by 5")
+# elif x%11 == 0:
+#     print("by 11")
 
-else:
-    print("by none")            
+# else:
+#     print("by none")            
 
+# x = int(input("enter:"))
+# for i in range(1,11):
+#      print(f(i*x) 
+
+
+# for i in range(1,101):
+#     if i%2== 0:
+#         print(i)
+
+x = [input("enter any 5 digit no."),input(),input(),input(),input()]
+copy = x.copy()
+copy.reverse()
+print(copy85635)
